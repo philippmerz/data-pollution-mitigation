bf71bc252af7894a90bcbe4e62688197a6249823
Jay <t.chou@student.tue.nl>
2024-12-18 15:46:31 +0100

    language removal

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Any
 import pandas as pd
 from src.config.config import Config
+from langdetect import detect_langs
 
 class DataPreprocessor:
     def __init__(self, config: Config):
@@ -18,4 +19,24 @@ class DataPreprocessor:
         # Eliminate all authors with less than 300 characters
         df = df.groupby('author_ID').filter(lambda x: len(''.join(x['post'])) > self.config.min_chars_per_author)
 
+        # Elimate all posts that are 80% another language
+        df = df[df['post'].apply(self.filter_language)]
+
         return df
+    
+    def filter_language(self, post, confidence_threshold=0.8):
+        predictions = detect_langs(post)
+
+        # Extract the most probable language and its confidence score
+        language = max(predictions, key=lambda x: x.prob)
+
+        # Is true if the language is English and the confidence score is above the threshold
+        is_english = language.lang == 'en' and language.prob > confidence_threshold
+        
+        # Print if the post is not english (only for debugging)
+        if not is_english:
+            print(f"Post not in English: {post}") 
+
+        return is_english
+
+
