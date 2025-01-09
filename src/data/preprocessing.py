1c6c9039f2c7e3d518c3770c0e72c3fef6bf72d2
Jay <t.chou@student.tue.nl>
2025-01-09 07:44:39 +0100

    regular expression replacement final

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -134,15 +134,15 @@ class DataPreprocessor:
     
 
     def filter_contamination(self, post):
-        # Define the regex patterns TODO move them to the config file
+        # Define the regex patterns 
         pattern1 = r"\b(?:I(?:'m|'m a| am| am a| identify as| identify as a)\s)(male|female|father|mother|brother|sister|daughter|son|man|women|guy|girl|boy|husband|wife)\b"  # For self-references
-        pattern2 = r"\b(\d{2})([MF])\b|\b([MF])(\d{2})\b"
+        pattern2 = r"\b(\d{2})([MFmf])\b|\b([MFmf])(\d{2})\b"
 
         # Replace self-references (e.g., "I am a male" -> "I am a human")
         post = re.sub(pattern1, lambda m: re.sub(r"\b(male|female|father|mother|brother|sister)\b", "human", m.group(0)), post)
             
         # Replace "22M" or "22F" with just the number (e.g., "22M" -> "22" and "22F" -> "22")
-        post = re.sub(pattern2, lambda m: m.group(1), post)  # Remove the 'M' or 'F' and keep the number
+        post = re.sub(pattern2, lambda m: m.group(1) if m.group(1) else m.group(4), post)  # Remove the 'M' or 'F' and keep the number
 
         matches = re.findall(pattern1, post) + re.findall(pattern2, post)
             
